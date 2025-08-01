184c223bd420847b0caf5024e2c7cb7cda6ad78c
lasya-danda <lasyadanda4406@gmail.com>
2025-08-01 21:50:55 +0530

    waterController.js

--- waterController.js
+++ waterController.js
@@ -1,1 +1,20 @@
-// waterController.js - placeholder
+const WaterUsage = require("../models/WaterUsage");
+
+exports.addUsage = async (req, res) => {
+  try {
+    const newEntry = new WaterUsage(req.body);
+    await newEntry.save();
+    res.status(201).json({ message: "Water usage recorded!" });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to save usage" });
+  }
+};
+
+exports.getUsage = async (req, res) => {
+  try {
+    const usageData = await WaterUsage.find();
+    res.json(usageData);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch usage data" });
+  }
+};
