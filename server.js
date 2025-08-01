7c9204a09df7cc7c5ce0133764b2bb8771f55326
lasya-danda <lasyadanda4406@gmail.com>
2025-08-01 21:50:18 +0530

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// server.js - placeholder
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
+
+const authRoutes = require("./routes/auth");
+const waterRoutes = require("./routes/waterRoutes");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/water", waterRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
