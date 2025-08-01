f516f88de0cd749838fb0e4f2de780985ec538ed
lasya-danda <lasyadanda4406@gmail.com>
2025-08-01 21:51:11 +0530

    waterRoutes.js

--- waterRoutes.js
+++ waterRoutes.js
@@ -1,1 +1,8 @@
-// waterRoutes.js - placeholder
+const express = require("express");
+const router = express.Router();
+const waterController = require("../controllers/waterController");
+
+router.post("/add", waterController.addUsage);
+router.get("/all", waterController.getUsage);
+
+module.exports = router;
